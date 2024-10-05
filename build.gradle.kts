264560603fe71143c70b8e0ca339cf0be39b92e0
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-05 11:42:54 +0300

    gradle dependencies added

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+
+}
